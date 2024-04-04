e4f251d901d09698f0fc89e06ec4978c1a61aa20
rayanfarhat <rayanfarhat.sh@gmail.com>
2024-04-04 23:49:26 +0300

    update with gitignore

--- client/src/core/gotoURL.ts
+++
@@ -1,7 +0,0 @@
-import { browser } from "$app/environment";
-
-export function gotoURL(url: string) {
-    if (browser) {
-        window.location.href = url;
-    }
-}
--- client/src/routes/+layout.ts
+++
@@ -1,3 +0,0 @@
-export const prerender = false; //محتاجو ضروري جدا
-export const ssr = false;
-export const csr = true;
